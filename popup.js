add1bb4394b668688ff1c6bb4966a685584a3a8b
Mohammad Umair <umair@testpress.in>
2018-04-16 12:17:03 +0530

    Change page background on button click

--- popup.js
+++ popup.js
@@ -9,3 +9,8 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+      chrome.tabs.executeScript(
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  };
